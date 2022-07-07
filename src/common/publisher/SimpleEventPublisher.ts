9cf3d9f65dac9a0bad712eeff31938c52b66f78a
psuwala <oneat@vp.pl>
2022-07-07 21:29:56 +0200

    created EventPublisher implementation

--- src/common/publisher/SimpleEventPublisher.ts
+++ src/common/publisher/SimpleEventPublisher.ts
@@ -1,1 +1,16 @@
-class SimpleEventPublisher extends EventPublisher{}
+import {EventPublisher} from "./EventPublisher";
+import {EventMessage} from "../messages/EventMessage";
+
+type EventSubscriber = (message: EventMessage) => Promise<void>
+
+export class SimpleEventPublisher implements EventPublisher {
+    subscribers: EventSubscriber[] = []
+
+    async publish(message: EventMessage): Promise<void> {
+        await Promise.all(this.subscribers.map(x => x(message)))
+    }
+
+    subscribe(callback: EventSubscriber): void {
+        this.subscribers.push(callback)
+    }
+}
