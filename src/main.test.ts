d43373c5eea5215dfafbc7f57ef750d282f30b10
KGrzeg <kupczykgrzeg@gmail.com>
2022-07-07 20:56:41 +0200

    Fresh new repo

---
+++ src/main.test.ts
@@ -0,0 +1,15 @@
+import main from "../src/main" 
+
+describe('JustMainJestTest', () => {
+  describe('do dis work?', () => {
+    let result = "";
+    beforeEach(() => {
+      console.log("bubble");
+      result = main();
+    });
+
+    it('it should return something', () => {
+      expect(result).toBe('Hi world');
+    });
+  });
+});
