6f1b1e5e0499b61573b25631feb7e7f1d97ec43f
Maciej Błędkowski <pub@mble.dk>
2022-07-10 00:19:48 +0200

    Add twitter integration

---
+++ src/twitter/main.ts
@@ -0,0 +1,20 @@
+import Twitter from "twitter-api-v2";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const client = new Twitter({
+  appKey: process.env.TWITTER_API_KEY ? process.env.TWITTER_API_KEY : "",
+  appSecret: process.env.TWITTER_API_KEY_SECRET
+    ? process.env.TWITTER_API_KEY_SECRET
+    : "",
+  accessToken: process.env.TWITTER_ACCESS_TOKEN
+    ? process.env.TWITTER_ACCESS_TOKEN
+    : "",
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    ? process.env.TWITTER_ACCESS_TOKEN_SECRET
+    : "",
+});
+
+export default async function sendTweet(tweetMessage: string) {
+  client.v1.tweet(tweetMessage).catch(console.error);
+}
