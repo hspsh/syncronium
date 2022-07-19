6d368fb72f4fdb4210b9c0d0144e4a1097cccfb4
LiquidLemon <liquidlemon@protonmail.com>
2022-07-19 22:31:16 +0200

    Simplify deploy script

--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -2,7 +2,7 @@
 set -x
 source scripts/commons.sh
 
-if [[ ! -z "$KUBECONFIG_DATA" ]]; then
+if [[ -n "$KUBECONFIG_DATA" ]]; then
   kubeconf=$(mktemp /tmp/kubeconf.XXXXX)
   echo "$KUBECONFIG_DATA" > $kubeconf
   export KUBECONFIG=$kubeconf
@@ -10,6 +10,6 @@ fi
 
 helm upgrade --install --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n syncronium --create-namespace syncronium ./helm
 
-if [[ ! -z "$kubeconf" ]]; then
+if [[ -n "$kubeconf" ]]; then
   rm $kubeconf
 fi
