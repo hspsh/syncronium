97007c8c69db51131b07c247c02664331483b976
psuwala <oneat@vp.pl>
2022-07-19 23:59:25 +0200

    created printing of variables

--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -9,6 +9,10 @@ if [[ -n "$KUBECONFIG_DATA" ]]; then
   export KUBECONFIG=$kubeconf
 fi
 
+echo "### VARIABLES ###"
+echo "K8S_NAMESPACE=$K8S_NAMESPACE"
+echo "DISCORD_GROUP=$DISCORD_GROUP"
+
 helm upgrade --install --set discordGroup=$DISCORD_GROUP --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n $K8S_NAMESPACE --create-namespace $K8S_NAMESPACE ./helm
 
 if [[ -n "$kubeconf" ]]; then
