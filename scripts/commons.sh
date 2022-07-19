1cf76253904e318df312ca55da0f69232771eaea
psuwala <oneat@vp.pl>
2022-07-19 23:35:25 +0200

    made DISCORD_GROUP into secrets

--- scripts/commons.sh
+++ scripts/commons.sh
@@ -4,7 +4,6 @@ function guten_tag(){
     echo `git rev-parse --short HEAD`
 }
 
-
 export IMAGE_TAG="${IMAGE_TAG:-$(guten_tag)}"
 export DATA_DIR="${DATA_DIR:-~/syncronium}"
 export REPO_NAME="${DOCKER_REPO:-ksidelta}"
--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -9,7 +9,7 @@ if [[ -n "$KUBECONFIG_DATA" ]]; then
   export KUBECONFIG=$kubeconf
 fi
 
-helm upgrade --install --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n syncronium --create-namespace syncronium ./helm
+helm upgrade --install --set discordGroup=$DISCORD_GROUP --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n syncronium --create-namespace syncronium ./helm
 
 if [[ -n "$kubeconf" ]]; then
   rm $kubeconf
