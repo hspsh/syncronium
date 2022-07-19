1b38a5f4abd2b3d0924946fe66643e2475263f8c
psuwala <oneat@vp.pl>
2022-07-19 21:26:52 +0200

    Prepared interface for persisted events

--- src/common/publisher/EventPublisher.ts
+++ src/common/publisher/EventPublisher.ts
@@ -3,5 +3,11 @@ import { EventMessage } from "../messages/EventMessage";
 export interface EventPublisher {
   publish(message: EventMessage): Promise<void>;
 
-  subscribe(callback: (message: EventMessage) => Promise<void>): void;
+  subscribe(
+    callback: (message: EventMessage, metadata: MetaEvent) => Promise<void>
+  ): void;
+}
+
+export interface MetaEvent {
+  id: number;
 }
--- src/common/publisher/SimpleEventPublisher.ts
+++ src/common/publisher/SimpleEventPublisher.ts
@@ -1,15 +1,21 @@
-import { EventPublisher } from "./EventPublisher";
+import { EventPublisher, MetaEvent } from "./EventPublisher";
 import { EventMessage } from "../messages/EventMessage";
 
-type EventSubscriber = (message: EventMessage) => Promise<void>;
+type EventSubscriber = (
+  message: EventMessage,
+  metaEvent: MetaEvent
+) => Promise<void>;
 
 export class SimpleEventPublisher implements EventPublisher {
   subscribers: EventSubscriber[] = [];
+  id: number = 0;
 
   async publish(message: EventMessage): Promise<void> {
-    await Promise.all(this.subscribers.map((x) => x(message))).catch(
-      (err) => `Failed to process ${message} because of ${err}`
-    );
+    const currentId = this.id++;
+
+    await Promise.all(
+      this.subscribers.map((x) => x(message, { id: currentId }))
+    ).catch((err) => `Failed to process ${message} because of ${err}`);
   }
 
   subscribe(callback: EventSubscriber): void {
