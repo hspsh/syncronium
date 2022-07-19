71179c2978b81a243a34dc389cefce6a80951b82
psuwala <oneat@vp.pl>
2022-07-19 23:41:13 +0200

    namespace syncronium from secrets

--- scripts/commons.sh
+++ scripts/commons.sh
@@ -7,5 +7,7 @@ function guten_tag(){
 export IMAGE_TAG="${IMAGE_TAG:-$(guten_tag)}"
 export DATA_DIR="${DATA_DIR:-~/syncronium}"
 export REPO_NAME="${DOCKER_REPO:-ksidelta}"
+export DISCORD_GROUP="${DISCORD_GROUP:-621300560481615892}"
+export K8S_NAMESPACE="${K8S_NAMESPACE:-syncronium}"
 
 export -f guten_tag
--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -9,7 +9,7 @@ if [[ -n "$KUBECONFIG_DATA" ]]; then
   export KUBECONFIG=$kubeconf
 fi
 
-helm upgrade --install --set discordGroup=$DISCORD_GROUP --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n syncronium --create-namespace syncronium ./helm
+helm upgrade --install --set discordGroup=$DISCORD_GROUP --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n $K8S_NAMESPACE --create-namespace $K8S_NAMESPACE ./helm
 
 if [[ -n "$kubeconf" ]]; then
   rm $kubeconf
