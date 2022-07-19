65187103ccc7cb008d4d9b23c22385499f0805a0
LiquidLemon <liquidlemon@protonmail.com>
2022-07-19 22:21:10 +0200

    Use $KUBECONFIG_DATA in helm-deploy.sh

--- scripts/helm-deploy.sh
+++ scripts/helm-deploy.sh
@@ -2,4 +2,14 @@
 
 source scripts/commons.sh
 
+if [[ ! -z "$KUBECONFIG_DATA" ]]; then
+  kubeconf=$(mktemp /tmp/kubeconf.XXXXX)
+  echo "$KUBECONFIG_DATA" > $kubeconf
+  export KUBECONFIG=$kubeconf
+fi
+
 helm upgrade --install --set discordApiKey=$DISCORD_API_KEY --set imageTag=$(guten_tag) -n syncronium --create-namespace syncronium ./helm
+
+if [[ ! -z "$kubeconf" ]]; then
+  rm $kubeconf
+fi
