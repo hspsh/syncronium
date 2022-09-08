abea3a78fed283d5b5cf9a5799ddfb4ef60edba9
LiquidLemon <liquidlemon@protonmail.com>
2022-09-08 18:41:38 +0200

    ci: set up deployments for gitops

--- scripts/commons.sh
+++ scripts/commons.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function guten_tag(){
-    echo `git rev-parse --short HEAD`
+    git describe --tags --abbrev=0 --exact-match HEAD 2>/dev/null \
+      || git rev-parse --short HEAD
 }
 
 export IMAGE_TAG="${IMAGE_TAG:-$(guten_tag)}"
 export DATA_DIR="${DATA_DIR:-~/syncronium}"
 export REPO_NAME="${DOCKER_REPO:-ksidelta}"
 export DISCORD_GROUP="${DISCORD_GROUP:-621300560481615892}"
-export K8S_NAMESPACE="${K8S_NAMESPACE:-syncronium}"
 
 export -f guten_tag
--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -3,4 +3,7 @@
 source ./scripts/commons.sh
 
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-docker push ${DOCKER_REPO}/syncronium:$(guten_tag) 
+docker push ${DOCKER_REPO}/syncronium:$(guten_tag)
+
+docker tag ${DOCKER_REPO}/syncronium:$(guten_tag) ${DOCKER_REPO}/syncronium:latest
+docker push ${DOCKER_REPO}/syncronium:latest
